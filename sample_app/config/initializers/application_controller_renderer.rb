b36687aaf10850284d8cb60256a675f06541e894
seasalt3745 <kh.seasaltice.3745@gmail.com>
2020-08-23 00:59:59 +0900

    [Finish]3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8241bb11aa3978eb7b8389cb78b59b1b98af18910ed6ee219f27a87f63db4b16f09639bb0d167b7fb7dcff535ba98c574020d2efaf370a93f10e85da0d7299ad'
